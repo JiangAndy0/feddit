4eb1762881102b835475664e191b2f11b8d2fc4b
Andy Jiang <andyjiangart@gmail.com>
2022-07-24 16:08:24 -0500

    added ability to style image containers differently based on the image

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { formatNum } from "../helperFunctions.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export const Post = ({ post, setActivePost, setShowingReplies, setShowingImage}) => {
+    const [hasTallImage, setHasTallImage] = useState(false);
+
     //sets the active post to this post, tells app to show replies module
     const handleCommentClick = () => {
         setActivePost(post);
@@ -15,12 +18,26 @@ export const Post = ({ post, setActivePost, setShowingReplies, setShowingImage})
         setActivePost(post);
         setShowingImage( true );
     }
+
+    //determine if the post's image is a portrait photo
+    const handleImageLoad = ({ target:img }) => {
+        const {offsetHeight, offsetWidth} = img;
+        console.log(offsetHeight);
+        if (offsetHeight > offsetWidth) {
+            setHasTallImage(true);
+        }
+    }
+
     return (
         <div className="post">
             <p>{post.timeAgo} ago by {post.author}</p>
             <h2>{post.title}</h2>
-            <div className='img-container'>
-                <img src={post.imgPreviewURL} onClick={handleImageClick}/>
+            <div className={hasTallImage ? 'img-container tall' : 'img-container wide'}>
+                <img 
+                    src={post.imgPreviewURL} 
+                    onClick={handleImageClick}
+                    onLoad={handleImageLoad}
+                />
             </div>
             <span className='stat'>
                 <FontAwesomeIcon icon={faArrowUp}/> {formatNum(post.score)} <FontAwesomeIcon icon={faArrowDown}/>
