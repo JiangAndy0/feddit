621a217b26b73f743111fef8d3a6aa3c819f6bc8
Andy Jiang <andyjiangart@gmail.com>
2022-07-25 14:04:49 -0500

    added proper formatting to replies

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -4,15 +4,23 @@ import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 export const Reply = ({ reply }) => {
     return (
         <div className="reply">
-            <div className="img-container">
-                <img src={reply.authorImg} />
+            <div className="left-pane">
+                <div className="img-container">
+                    <img src={reply.authorImg} />
+                </div>
+                <div className='h-line'></div>
             </div>
-            <p className='signature'>{reply.author} {reply.timeAgo} ago</p>
-            <p>{reply.body}</p>
-            <p className='stat'><FontAwesomeIcon icon={faArrowUp} /> {reply.score} <FontAwesomeIcon icon={faArrowDown} /></p>
-            {reply.replies && 
-                reply.replies.map( (reply, index) => <Reply reply={reply} key={reply.author + index}/>)
-            }
+            <div className="right-pane">
+                <div className='signature-container'>
+                    <p className='signature'>{reply.author} {reply.timeAgo} ago</p>
+                </div>
+                <p className='reply-body'>{reply.body}</p>
+                <p className='stat'><FontAwesomeIcon icon={faArrowUp} /> {reply.score} <FontAwesomeIcon icon={faArrowDown} /></p>
+                {reply.replies && 
+                    reply.replies.map( (reply, index) => <Reply reply={reply} key={reply.author + index}/>)
+                }
+            </div>
+            
         </div>
     );
 }
